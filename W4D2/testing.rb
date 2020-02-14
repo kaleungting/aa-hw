fb192d1e65cb1b752d678446db7a14ef1e4a2391
Ken Ting <kaleungting@gmail.com>
2020-02-14 01:22:19 -0500

    W4D5 hw

--- W4D2/testing.rb
+++ W4D2/testing.rb
@@ -1,64 +1,17 @@
-class Animal 
-    def initialize
+class Vehicle
 
-    end
-
-  def dominates?(other_animal)
-    self.secret_dominance_score > other_animal.secret_dominance_score
-  end
-
-  private
-  attr_reader :secret_dominance_score
-
-end
-
-class Dog < Animal
-  def initialize
-    @secret_dominance_score = rand
-  end
-
-  def boost
-    @secret_dominance_score + 1
+  attr_reader :wheels, :color
+  def initialize(wheels = 4,color = "black") 
+    @wheels = wheels
+    @color = color
   end
 
 end
 
-class Cat < Animal
-  def initialize
-    @secret_dominance_score = rand
+class MyCar < Vehicle
+
+  def initialize(bling)
+    super()
+    @bling = bling
   end
 end
-
-if __FILE__ == $PROGRAM_NAME
-koba = Dog.new  
-biscuit = Cat.new
-# puts koba.dominates?(biscuit) 
-puts koba.boost
-end
-
-# class Person  
-#   def initialize(age)  
-#     @age = age  
-#   end  
-#   def age  
-#     @age  
-#   end  
-#   def compare_age(c)  
-#     if c.age > age  
-#       "The other object's age is bigger."  
-#     else  
-#       "The other object's age is the same or smaller."  
-#     end  
-#   end  
-#   protected
-#   attr_reader :age  
-# end  
-
-
-# if __FILE__ == $PROGRAM_NAME
-# chris = Person.new(25)  
-# marcos = Person.new(34)  
-# puts chris.compare_age(marcos)  
-# #puts chris.age  
-
-# end
---
+++ W4D5/octupus_problems.rb
@@ -0,0 +1,78 @@
+require "byebug"
+
+fishies = ['fish', 'fiiish', "fiiiissshhhhhh",'fiiiiish' ,'fiiiish', 'fffish', 'ffiiiiisshh', 'fsh',]
+
+def sluggish_octopus(fishies)
+    # debugger
+    max = fishies[0]
+    (0...fishies.length).each do |idx1|
+        (idx1...fishies.length).each do |idx2|
+            if idx2 > idx1
+                if fishies[idx1].length > fishies[idx2].length
+                    max = fishies[idx1] if max.length < fishies[idx1].length && fishies[idx2].length
+                end
+            end
+        end
+    end
+    max
+end
+
+p sluggish_octopus(fishies)
+
+
+class Array
+    def merge_sort_dominant(&prc)
+        return self if self.length <= 1
+        mid = self.length/2
+        left = self.take(mid)
+        right = self.drop(mid)
+        left_sorted = left.merge_sort_dominant(&prc)
+        right_sorted = right.merge_sort_dominant(&prc)
+        Array.merge(left_sorted,right_sorted,&prc)
+    end
+
+    def self.merge(left,right,&prc)
+        prc || Proc.new {|a,b| a <=> b}
+        merged = []
+        until left.empty? || right.empty?
+            if prc.call(left.first,right.first) <= 0
+                merged << left.shift
+            else
+                merged << right.shift
+            end
+        end
+        merged + left + right
+    end
+end
+
+
+def merge_dominant_octopus(fishies)
+    prc ||= Proc.new {|a,b| a.length <=> b.length}
+    fishies.merge_sort_dominant(&prc).last
+end
+
+p merge_dominant_octopus(fishies)
+
+
+def clever_octopus(fishies)
+    max = fishies[0]
+    (0...fishies.length-1).each do |i|
+        if fishies[i+1].length > max.length
+            max = fishies[i+1]
+        end
+    end
+    max
+
+end
+
+p clever_octopus(fishies)
+
+tiles_array = ["up", "right-up", "right", "right-down", "down", "left-down", "left",  "left-up" ]
+
+def slow_dance(move,tiles_array)
+    tiles_array.each_with_index do |tile,idx|
+        return idx if tile == move
+    end
+end
+
+p slow_dance("up",tiles_array)
